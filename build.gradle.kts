2e17d10f8c182a8069936d411cdb871463c3aa78
Trilleo <gongsanle@gmail.com>
2026-04-19 10:58:52 +0800

    Improvement: Map version in plugin.yml to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,12 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
 }
 
+tasks.processResources {
+    filesMatching("*.yml") {
+        expand("projectVersion" to project.version)
+    }
+}
+
 tasks.jar {
     from(configurations.runtimeClasspath.get().map { zipTree(it) })
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
